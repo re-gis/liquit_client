c90776eb8f12ad4c76a5d51c220d392a7554677d
re-gis <irumvaregisdmc@gmail.com>
2023-12-17 22:10:05 +0200

    Making the sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import SideBar from "./components/SideBar";
+import Header from "./components/Header";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex h-[100%]">
+        <div className="w-[15%]  p-5">
+          <SideBar />
+        </div>
+        <div>
+          <div className="fixed">
+            <Header />
+          </div>
+          <Routes>
+            <Route path="/" />
+            <Route path="/borrow" />
+            <Route path="/stability" />
+            <Route path="/staking" />
+            <Route path="/lp" />
+            <Route path="/redemptions" />
+            <Route path="/liquidations" />
+            <Route path="/analytics" />
+            <Route path="/knowledge" />
+            <Route path="/tokens" />
+          </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header:React.FC = () => {
+  return (
+    <div>
+      this is a header
+    </div>
+  )
+}
+
+export default Header
